1fc022caef3d548dfb04fc118482b4af901c8be5
twinkle <twinkle18@navgurukul.org>
2020-02-04 16:46:53 +0530

    use proptype

--- src/componants/product.js
+++ src/componants/product.js
@@ -1,40 +1,54 @@
 import React, { Component } from 'react';
-import styled from 'styled-components'
-import {Link} from 'react-router-dom'
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 export default class Product extends Component {
-  render() {
-    const{id,title,img,price,inCart}=this.props.product
-    return (
-      <Productwrapper className="col-9 mx-auto col-md-6 col-lg-3 m-3"> 
-        <div className="card">
-          <div className="img-container p-5" onClick={(console.log("hello"))} >
-            <Link to="/detail">
-              <img src={img} alt="product" className="card-img-top"/>
-              </Link>
-              <button className="cart-btn" 
-              disabled={inCart?true:false}
-              onClick={()=>console.log("added")}>
-{inCart?(<p className="text-capitalize mb-0" disabled>
-  {""}
-  in Cart</p>):(<i className="fas fa-cart-plus"/>)}
-  </button>
-          </div>
-          <div className="card-footer d-flex">
-        <p className="align-self-center mb-0">
-          {title}
-          </p>
-          <h5 className="text-blue font-italic mb-0">
-            <span className="mr-1">$</span>
-            {price}
-            </h5>
-         </div>
-          </div>
-      </Productwrapper>
-    );
-  }
+	render() {
+		const { id, title, img, price, inCart } = this.props.product;
+		return (
+			<Productwrapper className="col-9 mx-auto col-md-6 col-lg-3 m-3">
+				<div className="card">
+					<div className="img-container p-5" onClick={console.log('hello')}>
+						<Link to="/detail">
+							<img src={img} alt="product" className="card-img-top" />
+						</Link>
+						<button
+							className="cart-btn"
+							disabled={inCart ? true : false}
+							onClick={() => console.log('added')}>
+							{inCart ? (
+								<p className="text-capitalize mb-0" disabled>
+									{''}
+									in Cart
+								</p>
+							) : (
+								<i className="fas fa-cart-plus" />
+							)}
+						</button>
+					</div>
+					<div className="card-footer d-flex">
+						<p className="align-self-center mb-0">{title}</p>
+						<h5 className="text-blue font-italic mb-0">
+							<span className="mr-1">$</span>
+							{price}
+						</h5>
+					</div>
+				</div>
+			</Productwrapper>
+		);
+	}
 }
-const Productwrapper=styled.div`
+Product.propTypes = {
+	product: PropTypes.shape({
+		id: PropTypes.number,
+		img: PropTypes.string,
+		title: PropTypes.string,
+		price: PropTypes.number,
+		inCart: PropTypes.bool,
+	}).isRequired
+};
+const Productwrapper = styled.div`
 // .card{
 //   border-color:transparent;
 //   transition:all 1s linear
@@ -79,4 +93,4 @@ const Productwrapper=styled.div`
   }
 
 }
-`
+`;
